7d77c185821f28d0d5ab0a349f9103f00e212740
Galochkin Sergey <galochkin@insigne.ru>
2021-10-31 18:59:31 +0300

    add unit test case DiscordWebhookService

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -2,9 +2,19 @@
 
 namespace Tests;
 
+use App\Services\DiscordWebhookService;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function testSendNotify()
+    {
+        // add notification
+        $this->json('post', '/api/notifications', ['who' => 'test', 'message' => 'test']);
+
+        $service = new DiscordWebhookService();
+        $this->assertNull($service->run(true));
+    }
 }
