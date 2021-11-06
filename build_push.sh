cae69b5adb71e908269d13c554e63b49ca44a2fc
Galochkin Sergey <galochkin@insigne.ru>
2021-11-06 20:31:27 +0300

    sh script pushh in dicker hub

---
+++ build_push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+cat ~/.docker_password.txt | docker login --username 13123123 --password-stdin
+
+docker build --no-cache --rm -t discor-notificator:latest .
+docker tag discor-notificator:latest 13123123/discor-notificator:latest
+docker push 13123123/discor-notificator:latest
