226515b32b99e00da3b71ad603284f15370b5d65
Galochkin Sergey <galochkin@insigne.ru>
2021-11-08 08:57:32 +0300

    sh script push in docker hub

--- build_push.sh
+++ build_push.sh
@@ -4,6 +4,6 @@ set -ex
 
 cat ~/.docker_password.txt | docker login --username 13123123 --password-stdin
 
-docker build --no-cache --rm -t discor-notificator:latest .
+docker build -f HubDockerfile --no-cache --rm -t discor-notificator:latest .
 docker tag discor-notificator:latest 13123123/discor-notificator:latest
 docker push 13123123/discor-notificator:latest
